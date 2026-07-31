19cb32e2c4bb33836bb4d74d3ff1c2a6ad7d69ff
yjh <kusatyd@naver.com>
2026-07-31 16:16:53 +0900

    기타 사항 수정, 프로젝트 등록

--- js/data.js
+++ js/data.js
@@ -1,5 +1,3 @@
-// 프로젝트 데이터
-// 새 프로젝트를 추가하려면 이 배열에 객체를 하나 더 넣으면 됩니다.
 const PROJECTS = [
   {
     title: '교통사고 위험 요인 분석',
@@ -14,9 +12,27 @@ const PROJECTS = [
     title: 'YOLO 기반 포트홀 감지 시스템',
     description:
       '도로 파손으로 인한 교통사고 및 유지보수의 비효율성 문제를 해결하고자 기반의 객체 YOLO 검출 시스템을 구축하였습니다.',
-    image: '#',
+    image: 'images/pothole_detact.png',
     tags: ['Pandas', 'Matplotlib', 'Python'],
-    demoUrl: null,
+    demoUrl:
+      'https://colab.research.google.com/drive/10AtdSNzbfZrE0abQDdohG2AZP2W-wq_M?authuser=0#scrollTo=UQ-O2AQZKO3h',
     codeUrl: 'https://github.com/yjh-311/2025_2_machine-learning/tree/main',
+  },
+  {
+    title: 'React 기반 쇼핑몰',
+    description: 'React를 사용하여 구현한 공예품 쇼핑몰입니다.',
+    image: 'images/react_shop.png',
+    tags: ['React', 'MySQL', 'Node.js', 'Express', 'JavaScript'],
+    demoUrl: null,
+    codeUrl: 'https://github.com/yjh-311/2025_react_shop',
+  },
+  {
+    title: '음악 특성 기반 유사 음악 클러스터링',
+    description:
+      'Spotify의 음악 3000곡의 정보를 사용하여 음악적 특징을 기준으로 클러스터링을 하여 유사한 음악끼리 분류한 프로젝트 입니다.',
+    image: 'images/music_cluster.png',
+    tags: ['Kaggle', 'python', 'matplotlib', 'Seaborn'],
+    demoUrl: null,
+    codeUrl: 'https://github.com/yjh-311/music-clustering/tree/main',
   },
 ]
